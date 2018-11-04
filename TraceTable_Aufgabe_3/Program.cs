a12d48feb1b7e46f35682e47e5932059aa83ecaf
Dominik Krahwinkel <dominik.krahwinkel@hs-furtwangen.de>
2018-11-04 13:06:34 +0100

    Stuff

---
+++ TraceTable_Aufgabe_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TraceTable_Aufgabe_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
