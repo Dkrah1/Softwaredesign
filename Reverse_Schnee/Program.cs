8cb5c6f966536e66c89fd2571ab48af494e0ecd0
Dominik Krahwinkel <dominik.krahwinkel@hs-furtwangen.de>
2018-11-18 07:03:32 +0100

    Aufgabe_5

---
+++ Reverse_Schnee/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Reverse_Schnee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Person! Please enter a sentence to fullfile the quest.");
+            string inputText = Console.ReadLine();
+
+            TurnAroundWord (inputText);
+            TurnAroundSentence(inputText);
+            TurnAroundWordAndSentence(inputText);
+
+            Console.WriteLine(TurnAroundWord(inputText));
+            Console.WriteLine("Ende");
+
+        }
+
+        public static string TurnAroundWord(string inputText)
+        {
+            string [] word = inputText.Split(' ');
+            Array.Reverse (word);
+            return string.Join (" ",word);
+        }
+
+        public static void TurnAroundSentence(string inputText)
+        {
+            string [] splitter = inputText.Split(new char());
+            for (int i = splitter.Length -1;i>=0; i--)
+            {
+                Console.WriteLine(splitter[i]);
+            }
+        }
+
+        public static string TurnAroundWordAndSentence(string inputText)
+        {
+            string [] splitter = inputText.Split(new char []{' '}, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = splitter.Length -1; i>0;i--)
+            {
+                char [] words = splitter[i].ToCharArray();
+                Array.Reverse(words);
+                Console.WriteLine(words);
+            }
+            return null;
+        }
+    }
+}
