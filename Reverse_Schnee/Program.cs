43f1892b8ce6fa86dc9ac4b34040c631a1a7fe22
Dominik Krahwinkel <dominik.krahwinkel@hs-furtwangen.de>
2018-11-21 10:16:01 +0100

    Update_Aufgabe_5

--- Reverse_Schnee/Program.cs
+++ Reverse_Schnee/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace Reverse_Schnee
 {
@@ -8,51 +10,61 @@ namespace Reverse_Schnee
         {
             Console.WriteLine("Hello Person! Please enter a sentence to fullfile the quest.");
             string inputText = Console.ReadLine();
-
-           string word = TurnAroundWord (inputText);
-            
-
-            Console.WriteLine(TurnAroundWord(inputText));
-            TurnAroundSentence(inputText);
+            TurnAroundLetterButKeepWordOrder(inputText);
+            TurnAroundWord(inputText);
             TurnAroundWordAndSentence(inputText);
-            Console.WriteLine("Ende");
+
+
+            Console.WriteLine(TurnAroundLetterButKeepWordOrder(inputText));
+            Console.WriteLine(TurnAroundWord(inputText));
+            Console.WriteLine(TurnAroundWordAndSentence(inputText));
+
 
         }
-
-       public static string TurnAroundWord(string inputText)
+        public static string TurnAroundLetterButKeepWordOrder(string inputText)
         {
-            char [] word = inputText.ToCharArray();             //Split(' ');
-            Array.Reverse (word);
-            inputText = string.Join (" ",word);
+            string reverse = TurnAroundWordAndSentence(inputText);
+            string reverse_two = TurnAroundWord(reverse);
+            return reverse_two;
+            /*List<string> listofWords = new List<string>();
+            string[] splitter = inputText.Split(' ');
+            for (int i = splitter.Length - 1; i >= 0; i--)
+            {
+                listofWords.Add(splitter[i]);
+                //Console.WriteLine("Sentence: " + splitter[i]);
+            }
+            listofWords.Reverse();
+            return string.Join("", listofWords.ToArray());*/
+        }
+        public static string TurnAroundWord(string inputText)
+        {
+            string[] word = inputText.Split(' ');             //Split(' ');
+            Array.Reverse(word);
+            inputText = string.Join(" ", word);
 
             return inputText;
-            
-           
-        }
 
-        public static void TurnAroundSentence(string inputText)
-        {
-            string [] splitter = inputText.Split(' ');
-            for (int i = splitter.Length -1;i>=0; i--)
-            {
-                Console.WriteLine("Sentence: " + splitter[i]);
-            }
-        }
 
+        }
         public static string TurnAroundWordAndSentence(string inputText)
         {
-            string [] splitter = inputText.Split(new char []{' '}, StringSplitOptions.RemoveEmptyEntries);
-            for (int i = splitter.Length -1; i>=0;i--)
+            char[] splitter = inputText.ToCharArray();            //(new char []{' '}, StringSplitOptions.RemoveEmptyEntries);
+            string reverse = String.Empty;
+            for (int i = splitter.Length - 1; i >= 0; i--)
             {
+                reverse += splitter[i];
+                //var listofSentence = reverse.ToString();
 
-                char [] words = splitter[i].ToCharArray();
+                /*char [] words = splitter[i].ToCharArray();
                 Array.Reverse(words);
-                Console.WriteLine("Letters and Words: ");
-                Console.WriteLine(words);
-                
+                //listofSentence.Add(words);
+                var list = words.ToString();
+                List<char> myList = new List<char>(words);
+                Console.WriteLine(myList);*/
+
 
             }
-            return null;
+            return reverse;
         }
     }
 }
