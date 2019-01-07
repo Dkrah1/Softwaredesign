4cc91ded07455ab4f113996eb576a972c2e27b8e
Dominik Krahwinkel <dominik.krahwinkel@hs-furtwangen.de>
2019-01-07 13:57:30 +0100

    Baum

---
+++ Generics/Main.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace L10
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new TreeNode<String>();
+            var root = tree.CreateNode("root");
+            var child1 = tree.CreateNode("child1");
+            var child2 = tree.CreateNode("child2");
+
+            root.AppendChild(child1);
+            root.AppendChild(child2);
+
+            var grand11 = tree.CreateNode("grand11");
+            var grand12 = tree.CreateNode("grand12");
+            var grand13 = tree.CreateNode("grand13");
+
+            child1.AppendChild(grand11);
+            child1.AppendChild(grand12);
+            child1.AppendChild(grand13);
+
+            var grand111 = tree.CreateNode("grand111");
+            var grand112 = tree.CreateNode("grand112");
+
+            grand11.AppendChild(grand111);
+            grand11.AppendChild(grand112);
+
+            var grand21 = tree.CreateNode("grand21");
+            
+            child2.AppendChild(grand21);
+            child1.RemoveChild(grand12);
+
+            root.PrintTree();            
+
+            foreach (var element in root.FindChild("grand111"))
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Generics
+{
+    public class TreeNode <T>
+    {
+        TreeNode <T> list = new TreeNode <T>();
+        
+        public TreeNode ()
+        {
+            var tree = new TreeNode <T>();
+            
+        }
+
+
+        public TreeNode<T> CreateNode(T name)
+        {
+            return; 
+        }
+
+        public void AppendChild(TreeNode<T> childNode)
+        {
+
+        }
+
+        public void RemoveChild(TreeNode<T> childNode)
+        {
+
+        }
+
+        public void MoveChild(TreeNode<T> childNode)
+        {
+
+        }
+
+        public void PrintTree(TreeNode<T> Node)
+        {
+
+        }
+
+    }
+}
